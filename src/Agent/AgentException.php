fabf81dcc2cbd928b652ffa62a3e5fd1cb5007b0
bublik <gbublik@gmail.com>
2019-11-08 22:34:04 +0300

    Start new version

--- src/Agent/AgentException.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace GBublik\Lpdt\Agent;
-
-class AgentException extends \Exception {
-
-}
--- src/RequestException.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace GBublik\Lpdt;
-
-class RequestException extends \Exception
-{
-
-}
